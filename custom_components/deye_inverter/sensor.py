545b0e626bea886c31901a6956f185cfcfbc69f2
jlopez77 <jauloso@gmail.com>
2025-05-06 22:04:14 +0200

    flake8 unused import

--- custom_components/deye_inverter/sensor.py
+++ custom_components/deye_inverter/sensor.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 import logging
-from typing import Any, Dict, List, Union
+from typing import Any, List, Union
 
 from homeassistant.components.sensor import (
     SensorDeviceClass,
